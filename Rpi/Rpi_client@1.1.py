8f5bb5ac5c4f6db88b9a3247b96281326742f807
Seunmul <geonhab504@gmail.com>
2022-09-07 13:26:24 +0900

    mid save

--- Rpi/Rpi_client@1.1.py
+++ Rpi/Rpi_client@1.1.py
@@ -1,7 +1,9 @@
 import json
 from threading import Thread
+from threading import Lock
 import socket
 import time
+from xmlrpc.client import boolean
 
 HOST = '155.230.25.98'
 # HOST = '127.0.0.1'
@@ -9,7 +11,9 @@ PORT = 9999
 
 # recivedData 전역변수 선언
 global recivedData
+recivedData = True
 # controller의 데이터를 서버로 전송
+
 
 
 def send_controller_data(client, status: str, X: float, Y: float, Z: float, W: float, R: float):
@@ -43,58 +47,111 @@ def send_connect_msg(client):
     return
 
 
-def Controller_Client(client):
-    try:
-        while True:
-            # recivedData 전역변수 사용
-            global recivedData
-            # dictionary type으로 받기
-            recivedData = json.loads(client.recv(1024).decode())
-            print(f"\n>> [C] received : \n{recivedData}")
-            if (recivedData["status"] == "detecting_finished"):
-                # detecting 중인 것을 서버에다가 알려야함.
-                send_controller_data(client, status="controlling",
-                                     X=0, Y=0, Z=0, W=0, R=0)
-                # echo 수신 후 동작
-                recivedData = json.loads(client.recv(1024).decode())
-                print(f"\n>> [D] received : \n{recivedData}")
-                # 작업 코드
-                print("\n\n\n\n ---- Controlling Arms ...---- \n\n\n\n")
-                time.sleep(3)
-                # 작업 코드 추가하면됩니다....
-                send_controller_data(client, status="controlling_finished",
-                                     X=10, Y=20, Z=30, W=40, R=50)
-            elif (recivedData["status"] == "stopping"):
-                print("stopping...")
-                time.sleep(3)
-                print("stopped")
-                
+def _control_(client, recivedData):
+    if (recivedData["status"] == "detecting_finished"):
+        # detecting 중인 것을 서버에다가 알려야함.
+        send_controller_data(client, status="controlling",
+                            X=0, Y=0, Z=0, W=0, R=0)
+        # echo 수신 후 동작
+        recivedData = json.loads(client.recv(1024).decode())
+        print(f"\n>> [D] received : \n{recivedData}")
+        # 작업 코드
+        print("\n\n\n\n ---- Controlling Arms ...---- \n\n\n\n")
+        time.sleep(3)
+        # 작업 코드 추가하면됩니다....
+        send_controller_data(client, status="controlling_finished",
+                            X=10, Y=20, Z=30, W=40, R=50)
+    return
 
-    except json.JSONDecodeError as e:
-        print(e)
-        print("잘못된 정보를 수신하였습니다.")
- 
+def _listener_(client) :
+    while True : 
+        try:
+            # dictionary type으로 받기
+            tempData = client.recv(1024)
+            if not tempData:
+                print(tempData)
+                raise ConnectionResetError()
+            recivedData = json.loads(tempData.decode())
+            print(recivedData)
+
+        except OSError as e:
+            print(e)
+            print(">> 소켓 서버 연결이 끊긴 것 같습니다. 프로그램을 종료합니다.")
+            print(">> input 'quit' to terminate program")
+            client.close()
+            break
+        except json.JSONDecodeError as e:
+            print(e)
+            print("잘못된 정보를 수신하였습니다.")
+        
+
+
+def Controller_Client(client):
+    # recivedData 전역변수 사용
+    global recivedData
+    isControlling = bool(False)
+    startListener = Thread(name="_listener_", target=_listener_,
+                                         args=(client, recivedData), daemon=True)
+    
+    # while True:
+    #     try:
+    #         # dictionary type으로 받기
+    #         tempData = client.recv(1024)
+    #         if not tempData:
+    #             print(tempData)
+    #             raise ConnectionResetError()
+    #         recivedData = json.loads(tempData.decode())
+
+    #     except OSError as e:
+    #         print(e)
+    #         print(">> 소켓 서버 연결이 끊긴 것 같습니다. 프로그램을 종료합니다.")
+    #         print(">> input 'quit' to terminate program")
+    #         client.close()
+    #         break
+    #     except json.JSONDecodeError as e:
+    #         print(e)
+    #         print("잘못된 정보를 수신하였습니다.")
+
+    #     else:
+    #         print(f"\n>> [C] received : \n{recivedData}")
+    #         if (recivedData["status"] == "stopping"):
+    #             print("stopping...")
+    #             print("stopped")
+    #             send_controller_data(client, status="stopping",
+    #                      X=0, Y=0, Z=0, W=0, R=0)
+    #         startingControl = Thread(name="_control_", target=_control_,
+    #                                      args=(client, recivedData), daemon=True)
+    #         startingControl.start()
+    #         startingControl.join()
+    #     finally:
+    #         pass
 
 
 if (__name__ == "__main__"):
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((HOST, PORT))
-    print('>> Connect Server')
-    send_connect_msg(client)
-    Controller_Client = Thread(name="Controller_Client", target=Controller_Client,
-                               args=(client,), daemon=True)
-    Controller_Client.start()
+    try:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((HOST, PORT))
+        print('>> Connect Server')
+        send_connect_msg(client)
+        Controller_Client = Thread(name="Controller_Client", target=Controller_Client,
+                                   args=(client,), daemon=True)
+        Controller_Client.start()
 
-    while True:
-        inputData = input('')
-        if inputData == 'quit':
-            print("exit client")
-            break
-        elif inputData == 'clear':
-            send_controller_data(
-                client, status=recivedData["status"], X=0, Y=0, Z=0, W=0, R=0)
-        elif inputData == 'td':
-            send_controller_data(
-                client, status=recivedData["status"], X=5, Y=5, Z=5, W=5, R=5)
-
-    client.close()
+        while True:
+            inputData = input('')
+            if inputData == 'quit':
+                print("exit client")
+                break
+            elif inputData == 'clear':
+                send_controller_data(
+                    client, status=recivedData["status"], X=0, Y=0, Z=0, W=0, R=0)
+            elif inputData == 'td':
+                send_controller_data(
+                    client, status=recivedData["status"], X=5, Y=5, Z=5, W=5, R=5)
+    except Exception as e:
+        print('에러는? : ', e)
+    except KeyboardInterrupt:
+        print('강제종료')
+    finally:
+        
+        client.close()
--- Tx2/Tx2_client@1.1.py
+++ Tx2/Tx2_client@1.1.py
@@ -28,8 +28,6 @@ def send_detector_data(client, status: str, classType: str,  accord_x: float, ac
     return
 
 # connect 메시지 전송 및 이니셜라이즈
-
-
 def send_connect_msg(client):
     sendingData = json.dumps({
         "status": "connecting",
@@ -40,57 +38,88 @@ def send_connect_msg(client):
     client.send(sendingData.encode())
     return
 
+def _control_(client, recivedData):
+    if (recivedData["status"] == "starting" or recivedData["status"] == "controlling_finished"):
+        # detecting 중인 것을 서버에다가 알려야함.
+        send_detector_data(client,  status="detecting", classType="resistor",
+                           accord_x=0, accord_y=0)
+        # echo 수신 후 동작
+        recivedData = json.loads(client.recv(1024).decode())
+        print(f"\n>> [D] received : \n{recivedData}")
+        # 작업 코드
+        print("\n\n\n\n ---- Detecting Elements......---- \n\n\n\n")
+        time.sleep(3)
+        # 작업 코드 추가하면됩니다....
+        send_detector_data(client, status="detecting_finished", classType="resistor",
+                           accord_x=30, accord_y=20)
+    return
 
 def Detector_Client(client):
-    try:
-        while True:
-            # recivedData 전역변수 사용
-            global recivedData
+    # recivedData 전역변수 사용
+    global recivedData
+
+    while True:
+        try:
             # dictionary type으로 받기
-            recivedData = json.loads(client.recv(1024).decode())
+            tempData = client.recv(1024)
+            if not tempData:
+                print(tempData)
+                raise ConnectionResetError()
+            recivedData = json.loads(tempData.decode())
+
+        except OSError as e:
+            print(e)
+            print(">> 소켓 서버 연결이 끊긴 것 같습니다. 프로그램을 종료합니다.")
+            print(">> input 'quit' to terminate program")
+            client.close()
+            break
+        except json.JSONDecodeError as e:
+            print(e)
+            print("잘못된 정보를 수신하였습니다.")
+            
+
+        else:
             print(f"\n>> [D] received : \n{recivedData}")
-            if (recivedData["status"] == "starting" or recivedData["status"] == "controlling_finished"):
-                # detecting 중인 것을 서버에다가 알려야함.
-                send_detector_data(client,  status="detecting", classType="resistor",
-                                   accord_x=0, accord_y=0)
-                # echo 수신 후 동작
-                recivedData = json.loads(client.recv(1024).decode())
-                print(f"\n>> [D] received : \n{recivedData}")
-                # 작업 코드
-                print("\n\n\n\n ---- Detecting Elements......---- \n\n\n\n")
-                time.sleep(3)
-                # 작업 코드 추가하면됩니다....
-                send_detector_data(client, status="detecting_finished", classType="resistor",
-                                   accord_x=30, accord_y=20)
-            elif (recivedData["status"] == "stopping"):
+            if (recivedData["status"] == "stopping"):
                 print("stopping...")
-                time.sleep(3)
                 print("stopped")
-
-    except json.JSONDecodeError as e:
-        print(e)
-        print("잘못된 정보를 수신하였습니다.")
-
+                send_detector_data(client, status="stopping", classType="resistor",
+                                   accord_x=30, accord_y=20)
+                continue
+            startingControl = Thread(name="_control_", target=_control_,
+                                         args=(client, recivedData), daemon=True)
+            startingControl.start()
+            startingControl.join()
+        finally:
+            pass
 
 
 if (__name__ == "__main__"):
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((HOST, PORT))
-    print('>> Connect Server')
-    send_connect_msg(client)
-    Detector_Client = Thread(name="Detector_Client", target=Detector_Client,
-                             args=(client,), daemon=True)
-    Detector_Client.start()
+    try:
+        # 클라이언트 소켓 생성
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((HOST, PORT))
+        print('>> Connect Server')
+        send_connect_msg(client)
+        Detector_Client = Thread(name="Detector_Client", target=Detector_Client,
+                                 args=(client,), daemon=True)
+        Detector_Client.start()
 
-    while True:
-        inputData = input('')
-        if inputData == 'quit':
-            print("exit client")
-            break
-        elif inputData == 'clear':
-            send_detector_data(client, status=recivedData["status"], classType="none",
-                               accord_x=0, accord_y=0)
-        elif inputData == 'td':
-            send_detector_data(client, status=recivedData["status"], classType="resistor",
-                               accord_x=60, accord_y=60)
-    client.close()
+        while True:
+            inputData = input('')
+            if inputData == 'quit':
+                print("exit client")
+                break
+            elif inputData == 'clear':
+                send_detector_data(client, status=recivedData["status"], classType="none",
+                                   accord_x=0, accord_y=0)
+            elif inputData == 'td':
+                send_detector_data(client, status=recivedData["status"], classType="resistor",
+                                   accord_x=60, accord_y=60)
+    except Exception as e:
+        print('에러는? : ', e)
+    except KeyboardInterrupt as e:
+        print('강제종료', e)
+    finally:
+       
+        client.close()
