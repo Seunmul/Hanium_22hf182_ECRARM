60b9e6de234f325a4d681299e551afb96a0f0b4f
seunmul <geonhab504@gmail.com>
2022-09-14 21:44:30 +0900

    removed legacy codes

--- Web_control_interface/flask_back/run_flask_server.py
+++ Web_control_interface/flask_back/run_flask_server.py
@@ -3,25 +3,8 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
-def first():
-    return render_template("login.html")
-
-@app.route("/login")
-def login():
-    return render_template("login.html")
-
-@app.route("/home")
-def home():
-    return render_template("home.html")
-
-@app.route("/status")
-def status():
-    return render_template("status.html")
-
-@app.route("/react")
 def my_index():
     return render_template("index.html", flask_token="react  ")
-
 
 
 if __name__ == "__main__":
