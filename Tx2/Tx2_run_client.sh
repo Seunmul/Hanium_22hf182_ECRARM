0363f6174edf67096a01d9966de3486bf2631fcc
seunmul <geonhab504@gmail.com>
2022-09-28 11:04:52 +0900

    작은 버그 수정 및 가중치 파일 추가
    yolov7.py=> sample .pt file

---
+++ Tx2/Tx2_run_client.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export WORK_HOME=$(pwd)/yolov7
+
+# yolov7 폴더 없으면 git clone https://github.com/WongKinYiu/yolov7.git yolov7 실행
+if [ ! -d ./yolov7 ]; then
+    echo NO yolov7 directory;
+    echo cloning yolov7 repository......;
+    git clone https://github.com/WongKinYiu/yolov7.git yolov7
+fi
+
+echo $WORK_HOME
+cp detect_custom.py yolov7/
+python Tx2_client@2.2.py
+#실행 안되면 python3 명령어로(리눅스,맥 등..)
