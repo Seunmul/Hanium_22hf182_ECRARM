d1353f1e78a3d569d1523b2a0ec79191faa2b9c0
seunmul <geonhab504@gmail.com>
2022-10-01 17:31:46 +0900

    Tx2_client@3.0->3.1
    카메라 로드 로직 수정
    
    
    Former-commit-id: 6f7f04fa9294a7e0866c951c1d2e1d5fde0d63ce

--- Tx2/cameraTest2.py
+++ Tx2/cameraTest2.py
@@ -2,7 +2,7 @@
 import cv2 # OpenCV(실시간 이미지 프로세싱) 모듈
 
 # 카메라 index 번호
-camera = 0
+camera = 2
 
 # VideoCapture : 카메라 열기
 capture = cv2.VideoCapture(camera)
@@ -13,8 +13,8 @@ h = capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
 print("원본 동영상 너비(가로) : {}, 높이(세로) : {}".format(w, h))
 
 # 동영상 크기 변환
-capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1280) # 가로
-capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 720) # 세로
+capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1920) # 가로
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080) # 세로
 
 # 변환된 동영상 크기 정보
 w = capture.get(cv2.CAP_PROP_FRAME_WIDTH)
@@ -26,6 +26,7 @@ from threading import Thread
 def _listener_():
     while True: 
         doCapture=input("캡쳐?")
+
         if(doCapture=='y'):
             print("캡쳐")
             cv2.imwrite('images/img_captured_test.jpg', frame, params=[cv2.IMWRITE_JPEG_QUALITY,100])
@@ -40,11 +41,11 @@ while True:
     # 1) 읽은 결과(True / False)
     # 2) 읽은 프레임
     retval, frame = capture.read()
-
-   
+    
     # 읽은 프레임이 없는 경우 종료
     if not retval:
         break
+   
     
     # 프레임 출력
     cv2.imshow("resize_frame", frame)
